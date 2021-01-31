898b4fcee6c24a6376532e7eca13b8ecebc64fec
Samuel Taylor <s.taylor19@lancaster.ac.uk>
2021-01-31 21:40:04 +0000

    Master script that runs all the relevant code to regenerate output - noting that output is already in the GitHub repo

--- Runall.R
+++ Runall.R
@@ -3,5 +3,17 @@
 
 library(here)
 
-source()
-...
+source(here("1_scripts_response_curves/082005summarize.R"))
+
+source(here("2_scripts_sun_shade_sun/082005NaturePlantsnlmeVcmax.R"))
+
+source(here("2_scripts_sun_shade_sun/082005NaturePlantsnlmeLightResponses.R"))
+
+source(here("2_scripts_sun_shade_sun/082005NaturePlantsnlmeActivationState.R"))
+
+source(here("3_scripts_diurnal_cowpea/082005NaturePlantsdiurnalmodels.R"))
+
+#Figures
+source(here("2_scripts_sun_shade_sun/082005NaturePlantsFigure1.R"))
+
+source(here("3_scripts_diurnal_cowpea/082005NaturePlantsFigure2.R"))
